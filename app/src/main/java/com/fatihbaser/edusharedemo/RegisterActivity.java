4ea9c7a732884225a11f6a23f13309baa0761990
Fatih Baser <basese_aehit@students.vizja.pl>
2021-09-16 14:07:30 +0300

    Initial commit

---
+++ app/src/main/java/com/fatihbaser/edusharedemo/RegisterActivity.java
@@ -0,0 +1,83 @@
+package com.fatihbaser.edusharedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+    Button back;
+    TextInputEditText mTextInputUsername;
+    TextInputEditText mTextInputEmail;
+    TextInputEditText mTextInputPassword;
+    TextInputEditText mTextInputConfirmPassword;
+    Button mButtonRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        back = findViewById(R.id.back);
+        mTextInputEmail = findViewById(R.id.textInputEmail);
+        mTextInputUsername = findViewById(R.id.textInputUsername);
+        mTextInputPassword = findViewById(R.id.textInputPassword);
+        mTextInputConfirmPassword = findViewById(R.id.textInputConfirmPassword);
+        mButtonRegister = findViewById(R.id.btnRegister);
+
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void register() {
+        String username = mTextInputUsername.getText().toString();
+        String email = mTextInputEmail.getText().toString();
+        String password  = mTextInputPassword.getText().toString();
+        String confirmPassword = mTextInputConfirmPassword.getText().toString();
+
+        if (!username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !confirmPassword.isEmpty()) {
+            if (isEmailValid(email)) {
+                Toast.makeText(this, "Insertaste todos los campos y el email es valido", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "Insertaste todos los campos pero el correo no es valido", Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            Toast.makeText(this, "Para continuar inserta todos los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /*
+     * VERIFICAR QUE SEA UN EMAIL VALIDO
+     */
+    public boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
