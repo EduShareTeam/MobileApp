90fcdb0c931873359a5818eb7eebb1893dba21c6
Fatih Baser <basese_aehit@students.vizja.pl>
2021-09-18 15:49:47 +0300

    PostActivity design is completed  and test successful

--- app/src/main/java/com/fatihbaser/edusharedemo/fragments/HomeFragment.java
+++ app/src/main/java/com/fatihbaser/edusharedemo/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.fatihbaser.edusharedemo.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,12 +10,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.fatihbaser.edusharedemo.R;
+import com.fatihbaser.edusharedemo.activities.PostActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class HomeFragment extends Fragment {
 
 
-
+    View mView;
+    FloatingActionButton mFab;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -30,7 +34,20 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.fragment_home, container, false);
+        mFab = mView.findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToPost();
+            }
+        });
+        return mView;
+    }
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    private void goToPost() {
+        Intent intent = new Intent(getContext(), PostActivity.class);
+        startActivity(intent);
     }
 }
