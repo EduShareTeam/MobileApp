fb08fe03fa51a27ec2a0bdefba7d9b6df622a848
yusufgltc <ygultac@hotmail.com>
2021-09-24 14:15:34 +0200

    Implemented searchbar for home fragment

--- app/src/main/java/com/fatihbaser/edusharedemo/fragments/HomeFragment.java
+++ app/src/main/java/com/fatihbaser/edusharedemo/fragments/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -21,74 +22,100 @@ import com.fatihbaser.edusharedemo.R;
 import com.fatihbaser.edusharedemo.activities.MainActivity;
 import com.fatihbaser.edusharedemo.activities.PostActivity;
 import com.fatihbaser.edusharedemo.adapter.PostsAdapter;
+import com.fatihbaser.edusharedemo.databinding.FragmentHomeBinding;
+import com.fatihbaser.edusharedemo.databinding.FragmentProfileBinding;
 import com.fatihbaser.edusharedemo.models.Post;
 import com.fatihbaser.edusharedemo.providers.AuthProvider;
 import com.fatihbaser.edusharedemo.providers.PostProvider;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.Query;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 
 
-public class HomeFragment extends Fragment {
-    View mView;
-    FloatingActionButton mFab;
-    Toolbar mToolbar;
+public class HomeFragment extends Fragment implements MaterialSearchBar.OnSearchActionListener {
+    private FragmentHomeBinding binding;
     AuthProvider mAuthProvider;
     PostProvider mPostProvider;
-    RecyclerView mRecyclerView;
     PostsAdapter mPostsAdapter;
+    PostsAdapter mPostsAdapterSearch;
 
     public HomeFragment() {
         // Required empty public constructor
     }
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        mView = inflater.inflate(R.layout.fragment_home, container, false);
-        mFab = mView.findViewById(R.id.fab);
-        mToolbar = mView.findViewById(R.id.toolbar);
-        mRecyclerView = mView.findViewById(R.id.recyclerViewHome);
-        //TODO: GridLayout'a cevir
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        mRecyclerView.setLayoutManager(linearLayoutManager);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        setHasOptionsMenu(true);
 
-        ((AppCompatActivity) getActivity()).setSupportActionBar(mToolbar);
-        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Paylasimlar");
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        binding.recyclerViewHome.setLayoutManager(linearLayoutManager);
         setHasOptionsMenu(true);
         mAuthProvider = new AuthProvider();
         mPostProvider = new PostProvider();
 
-        mFab.setOnClickListener(new View.OnClickListener() {
+        binding.searchBar.setOnSearchActionListener(this);
+        binding.searchBar.inflateMenu(R.menu.main_menu);
+        binding.searchBar.getMenu().setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.itemLogout) {
+                    logout();
+                }
+                return true;
+            }
+        });
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 goToPost();
             }
         });
-        return mView;
+        return view;
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
+    private void searchByTitle(String title) {
+        Query query = mPostProvider.getPostByTitle(title);
+        FirestoreRecyclerOptions<Post> options =
+                new FirestoreRecyclerOptions.Builder<Post>()
+                        .setQuery(query, Post.class)
+                        .build();
+        mPostsAdapterSearch = new PostsAdapter(options, getContext());
+        mPostsAdapterSearch.notifyDataSetChanged();
+        binding.recyclerViewHome.setAdapter(mPostsAdapterSearch);
+        mPostsAdapterSearch.startListening();
+    }
+
+    private void getAllPost() {
         Query query = mPostProvider.getAll();
         FirestoreRecyclerOptions<Post> options =
                 new FirestoreRecyclerOptions.Builder<Post>()
                         .setQuery(query, Post.class)
                         .build();
         mPostsAdapter = new PostsAdapter(options, getContext());
-        mRecyclerView.setAdapter(mPostsAdapter);
+        mPostsAdapter.notifyDataSetChanged();
+        binding.recyclerViewHome.setAdapter(mPostsAdapter);
         mPostsAdapter.startListening();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getAllPost();
     }
 
     @Override
     public void onStop() {
         super.onStop();
         mPostsAdapter.stopListening();
+        if (mPostsAdapterSearch != null) {
+            mPostsAdapterSearch.stopListening();
+        }
     }
 
     private void goToPost() {
@@ -96,25 +123,27 @@ public class HomeFragment extends Fragment {
         startActivity(intent);
     }
 
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        inflater.inflate(R.menu.main_menu, menu);
-        super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.itemLogout) {
-            logout();
-        }
-
-        return true;
-    }
-
     private void logout() {
         mAuthProvider.logout();
         Intent intent = new Intent(getContext(), MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+    }
+
+    @Override
+    public void onSearchStateChanged(boolean enabled) {
+        if (!enabled) {
+            getAllPost();
+        }
+    }
+
+    @Override
+    public void onSearchConfirmed(CharSequence text) {
+        searchByTitle(text.toString().toLowerCase());
+    }
+
+    @Override
+    public void onButtonClicked(int buttonCode) {
+
     }
 }
